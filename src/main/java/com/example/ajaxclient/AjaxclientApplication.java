4588d0159f6c80827a50b6b1b84f4fba635fb80e
yingpeng <yingpeng@iflytek.com>
2018-07-17 09:00:33 +0800

    ajax跨域问题前端界面

---
+++ src/main/java/com/example/ajaxclient/AjaxclientApplication.java
@@ -0,0 +1,12 @@
+package com.example.ajaxclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjaxclientApplication.class, args);
+    }
+}
